c27fe566ef16cf9865ba87c28c939861d12bad4d
Jean-PierrePapin <skouo82@gmail.com>
2020-06-16 18:14:04 +0100

    Built the Search controller

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -14,10 +14,27 @@ export default class Search {
             // console.log(this.result);
         } catch (error) {
             alert(error);
-        }
-        
+        }  
     }
-
-
 }
 
+
+// From rapidAPI
+/* 
+
+fetch("https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi", {
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
+		"x-rapidapi-key": "142ed31e65msh89a1814f8244311p1a1289jsn728ed500b1ca",
+		"content-type": "text/plain",
+		"accept": "text/plain"
+	},
+	"body": "https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/"
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+}); */
