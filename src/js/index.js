b2cf8df760730c40e4e49f91ab7d430fa212ace1
Jean-PierrePapin <skouo82@gmail.com>
2020-06-20 09:59:57 +0100

    Building the Recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -33,12 +33,18 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4) Search for recipes
-        await state.search.getResults();
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
 
-        // 5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
+        
     }
 }
 
@@ -61,5 +67,40 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * RECIPE CONTROLLER
  */
-const r = new Recipe();
-r.getRecipe();
+
+/* const r = new Recipe();
+r.getRecipe(); */
+const controlRecipe = () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+             // Get recipe data
+            await state.recipe.getRecipe();
+
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            console.log(state.recipe);
+        } catch (err) {
+            alert('Error processing recipe!');
+        }
+       
+    }
+};
+
+
+/* window.addEventListener('hashchange', controlRecipe);
+window.addEventListener('load', controlRecipe); */
+
+// forEach loop to pass several event with a addEventListener
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
